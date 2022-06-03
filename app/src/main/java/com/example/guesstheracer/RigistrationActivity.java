117e1da04cb3b17226eea6fbc272bf6e2b145f5d
Denis <denis_kuza2001@mail.ru>
2022-06-03 20:08:35 +0300

    initial commit

---
+++ app/src/main/java/com/example/guesstheracer/RigistrationActivity.java
@@ -0,0 +1,14 @@
+package com.example.guesstheracer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RigistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rigistration);
+    }
+}
