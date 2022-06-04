4473b59da33336562643d126fa9a225367b38ce7
Denis <denis_kuza2001@mail.ru>
2022-06-04 20:37:27 +0300

    SignUp buttons is work

--- app/src/main/java/com/example/guesstheracer/RegistrationActivity.java
+++ app/src/main/java/com/example/guesstheracer/RegistrationActivity.java
@@ -7,28 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
 
         signUpButton = (Button) findViewById(R.id.signup_button);
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-
     }
-
-
-
-
-
 }
