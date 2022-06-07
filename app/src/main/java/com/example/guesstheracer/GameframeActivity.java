5c9fa6d2819a046efc28aabb8fe13664b649f676
Denis <denis_kuza2001@mail.ru>
2022-06-07 15:09:30 +0300

    getting data from Firebase added

--- app/src/main/java/com/example/guesstheracer/GameframeActivity.java
+++ app/src/main/java/com/example/guesstheracer/GameframeActivity.java
@@ -1,5 +1,6 @@
 package com.example.guesstheracer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
@@ -12,16 +13,21 @@ import android.util.Log;
 import android.widget.Chronometer;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 public class GameframeActivity extends AppCompatActivity {
 
-    FirebaseDatabase db;
-
+    String TAG = "Firebase1";
+    Pair pair;
+    DatabaseReference mDatabase;
     ImageView imgView;
 
     @Override
@@ -29,10 +35,12 @@ public class GameframeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameframe);
 
-        db = FirebaseDatabase.getInstance();
+        //getting reference on Firebase database to extract data
+        mDatabase = FirebaseDatabase.getInstance("https://guess-the-racer-default-rtdb.europe-west1.firebasedatabase.app").getReference();
 
         imgView = (ImageView) findViewById(R.id.game_img);
 
+        getDataFromFirebase();
 
     }
 
@@ -53,10 +61,27 @@ public class GameframeActivity extends AppCompatActivity {
 
     }
 
-   /* private String getValue(FirebaseDatabase _db, String key){
-        DatabaseReference ref = db.getReference(key);
+    //why don't work if res is local variable(asking to make it final)?
+    Map<String, String> res;
+
+    //returns Map<String, String> contains Names and pic names
+    private Map<String, String> getDataFromFirebase(){
+
+        mDatabase.child("f1").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(task.isSuccessful()){
+                    res = (Map<String, String>) task.getResult().getValue();
+                    Log.d(TAG, "Successful get:" + res);
+                } else { Log.e(TAG, "Error getting data", task.getException());}
+            }
+        });
+
+        if(!res.isEmpty()) {
+            return res;
+        } else {return null; }
     }
-*/
+
 
     private void setImage(String name){
         try {
@@ -70,5 +95,15 @@ public class GameframeActivity extends AppCompatActivity {
 
     private Drawable getDrawableFromAssets(String path) throws IOException {
         return Drawable.createFromStream(getAssets().open(path), null);
+    }
+}
+
+class Pair{
+    String key;
+    String value;
+
+    Pair(String key, String value){
+        this.key = key;
+        this.value = value;
     }
 }
